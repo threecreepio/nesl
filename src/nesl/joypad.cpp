20ff1f2f0950ab36cd1bf1963f938f28f1539b59
threecreepio <me@creep.io>
2021-02-07 19:18:24 +0100

    add some joypad functions

--- src/nesl/joypad.cpp
+++ src/nesl/joypad.cpp
@@ -13,11 +13,25 @@ static int joypad_get_fake(lua_State* L) {
     return 1;
 }
 
+static int joypad_get_raw(lua_State* L) {
+    int which = luaL_checkinteger(L, 1) - 1;
+    if (which < 0 || which > 1) {
+        return luaL_error(L, "Invalid input port (valid range 1-4, specified %d)", which);
+    }
+    lua_pushinteger(L, NES->emu.current_joypad[which % 2]);
+    return 1;
+}
+
 static int joypad_set(lua_State* L) {
     // Which joypad we're tampering with
     int which = luaL_checkinteger(L, 1) - 1;
     if (which < 0 || which > 3) {
         return luaL_error(L, "Invalid output port (valid range 1-4, specified %d)", which);
+    }
+
+    if (lua_isnumber(L, 2)) {
+        NES->emu.current_joypad[which % 2] = lua_tointeger(L, 2);
+        return 0;
     }
 
     // And the table of buttons.
@@ -40,6 +54,7 @@ static int joypad_set(lua_State* L) {
 
 static const struct luaL_reg joypadlib[] = {
     {"get", joypad_get_fake},
+    {"getraw", joypad_get_raw},
     {"getdown", joypad_get_fake},
     {"getup", joypad_get_fake},
     {"getimmediate", joypad_get_fake},
